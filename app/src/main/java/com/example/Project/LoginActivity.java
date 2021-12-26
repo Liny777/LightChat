086df3418afc753290b2c650b4560413af6997df
Liny777 <1093742432@qq.com>
2021-12-27 05:22:05 +0800

    FCM & SpringBoot

--- app/src/main/java/com/example/Project/LoginActivity.java
+++ app/src/main/java/com/example/Project/LoginActivity.java
@@ -1,14 +1,22 @@
 package com.example.Project;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +24,24 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("LOGIN");
         setSupportActionBar(toolbar);
+        Log.d("Hello","LoginActivity");
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, msg);
+                        Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     @Override
---
+++ app/src/main/java/com/example/Project/MyFirebaseMessagingService.java
@@ -0,0 +1,136 @@
+package com.example.Project;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "MyFirebaseMessaging";
+    @Override
+    // This callback function will be called when an FCM message is received (except for a notification message is received when app is in background)
+    // Once the data is received, your app can:
+    // Generation a notification
+    // Jump to a new page
+    // Send request to server to fetch new data
+    // 瘦到服务器消息，然后显示通知
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            String msg = remoteMessage.getData().get("Msg");
+            String title = remoteMessage.getData().get("Title");
+            String id = remoteMessage.getData().get("Id");
+            sendNotification(id,title, msg);
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use WorkManager.
+                scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                handleNow();
+            }
+        }
+    }
+    // 产生新的Token
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+        sendRegistrationToServer(token);
+    }
+    // 注册后，将Token发送给服务器存储
+    private void sendRegistrationToServer(String token) {
+        // Implement your own logic to submit token to server
+        FormBody formBody = new FormBody.Builder()
+                .add("token", token)
+                .add("user_id", "1155169171")
+                .build();
+        Request postMessages = new Request.Builder()
+                .url("http://47.250.43.42/api/a4/submit_push_token")
+                .post(formBody)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(postMessages).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(@NonNull okhttp3.Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+            @Override
+            public void onResponse(@NonNull okhttp3.Call call, @NonNull okhttp3.Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String data = response.body().string();
+                    try {
+                        //setResponse(new JSONObject(data));
+                        System.out.println(new JSONObject(data));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+    }
+
+    // 显示通知的方法
+    public void sendNotification(String id,String title, String context) {
+
+        String CHANNEL_ID = getString(R.string.default_notification_channel_id);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification)
+                .setContentTitle(title)
+                .setContentText(context)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_CALL)
+                .setAutoCancel(true);
+
+
+        Intent indent = new Intent(this, ChatActivity.class);
+        indent.putExtra("room_title",title);
+        indent.putExtra("room_id",id);
+        indent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+        indent.addCategory(Intent.CATEGORY_LAUNCHER);
+        // Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(indent);
+        // Get the PendingIntent containing the entire back stack
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_IMMUTABLE);
+        builder.setContentIntent(pendingIntent);
+        builder.setFullScreenIntent(pendingIntent, true);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+        notificationManager.notify(0, builder.build());
+
+    }
+    private void scheduleJob() { }
+
+    private void handleNow() {
+        Log.d(TAG, "Short lived task is done.");
+    }
+}
